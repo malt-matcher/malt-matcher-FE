7d56d784fffbb1bdb69cfcd0e26ab69a21e37968
Jordan Farelli <jordanfarelli@gmail.com>
2022-10-14 15:27:59 -0600

    Trying to fix CircleCI errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar/NavBar';
-import MaltFinderContainer from './components/MaltFinderContainer/MaltFinderContainer';
-import BreweryLocationContainer from './components/BreweryLocationContainer/BreweryLocationContainer';
-import BeerListContainer from './components/BeerListContainer/BeerListContainer';
-import BreweryDetails from './components/BreweryDetails/BreweryDetails';
-import Footer from './components/Footer/Footer';
-import ErrorPage from './components/ErrorPage/ErrorPage';
+// import NavBar from './components/NavBar/NavBar';
+// import MaltFinderContainer from './components/MaltFinderContainer/MaltFinderContainer';
+// import BreweryLocationContainer from './components/BreweryLocationContainer/BreweryLocationContainer';
+// import BeerListContainer from './components/BeerListContainer/BeerListContainer';
+// import BreweryDetails from './components/BreweryDetails/BreweryDetails';
+// import Footer from './components/Footer/Footer';
+// import ErrorPage from './components/ErrorPage/ErrorPage';
 
 function App() {
   return (
