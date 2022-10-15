8f3729d158963b7af87c853b0bbcbbd05df93180
Cole Stephenson <colestephenson94@gmail.com>
2022-10-15 11:41:03 -0600

    Create and style error component

--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -1,8 +1,19 @@
 import "./ErrorPage.css";
 import React from "react";
+import {Link} from 'react-router-dom'
+import spilled from "../../assets/spilled.jpeg";
+
 
 const ErrorPage = () => {
-  return <div></div>;
+  return (
+    <div className="container">
+      <h2>Party Foul! Invalid URL. Please click button below to return home</h2>
+      <div className="img-container">
+        <img src={spilled} className="spilled-image" alt='spilled-beer' />
+      </div>
+      <Link to='/'><button className='home-button'>Home</button></Link>
+    </div>
+  )
 };
 
 export default ErrorPage;
