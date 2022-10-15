c4b9b346885071ba4d38ba0b588bd4808324a4f3
Cole Stephenson <colestephenson94@gmail.com>
2022-10-15 11:01:08 -0600

    Implement Router to application

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
-// import NavBar from './components/NavBar/NavBar';
-// import MaltFinderContainer from './components/MaltFinderContainer/MaltFinderContainer';
-// import BreweryLocationContainer from './components/BreweryLocationContainer/BreweryLocationContainer';
-// import BeerListContainer from './components/BeerListContainer/BeerListContainer';
-// import BreweryDetails from './components/BreweryDetails/BreweryDetails';
-// import Footer from './components/Footer/Footer';
-// import ErrorPage from './components/ErrorPage/ErrorPage';
+import NavBar from './components/NavBar/NavBar';
+import MaltFinderContainer from './components/MaltFinderContainer/MaltFinderContainer';
+import BreweryLocationContainer from './components/BreweryLocationContainer/BreweryLocationContainer';
+import BeerListContainer from './components/BeerListContainer/BeerListContainer';
+import BreweryDetails from './components/BreweryDetails/BreweryDetails';
+import Footer from './components/Footer/Footer';
+import ErrorPage from './components/ErrorPage/ErrorPage'
+import LandingPage from './components/LandingPage/LandingPage'
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route exact path='/' render={() => <LandingPage/>}/>
+        <Route exact path='/maltFinder' render={() => <MaltFinderContainer/>}/>
+        <Route exact path='/:breweryLocations' render={() => <BreweryLocationContainer/>}/>
+        <Route exact path='/:breweryLocations/:selectedBrewery' render={() => <BeerListContainer/>}/>
+        <Route exact path='/:breweryLocations/:selectedBrewery/details' render={() => <BreweryDetails/>}/>
+        <Route render={() => <ErrorPage/>}/>
+      </Switch>
+      <Footer />
     </div>
   );
 }
