336165acce32b2bc163e19008a4da9dd06d71819
Cole Stephenson <colestephenson94@gmail.com>
2022-10-17 15:09:06 -0600

    Force pass circleci

--- src/App.js
+++ src/App.js
@@ -17,16 +17,16 @@ function App() {
   return (
     <div className="App">
       <Link to='/'><NavBar /></Link>
-      {/* <Switch>
+       <Switch>
         <Route exact path='/' render={() => <LandingPage/>}/>
         <Route exact path='/maltFinder' render={() => <MaltFinderContainer/>}/>
         <Route exact path='/search/:breweryLocations' render={() => <BreweriesContainer/>}/>
         <Route exact path='/search/:breweryLocations/:selectedBrewery' render={() => <BeerListContainer/>}/>
         <Route exact path='/search/:breweryLocations/:selectedBrewery/details' render={() => <BreweryDetails/>}/>
         <Route render={() => <ErrorPage/>}/>
-      </Switch> */}
-      <AboutUs />
-      {/* <Footer /> */}
+      </Switch>
+      // <AboutUs />
+      /* <Footer /> */
     </div>
   );
 }
