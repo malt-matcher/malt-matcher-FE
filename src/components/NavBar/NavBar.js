b0cc129ea397b671ce882d1b7747c02a4175e446
Jordan Farelli <jordanfarelli@gmail.com>
2022-10-17 15:01:29 -0600

    Added and styled AboutUs component

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,11 +3,16 @@ import React from "react";
 
 const NavBar = () => {
   return (
-  <div className="navbar-container">
-    <h1 className="malt">Malt</h1>
-    <h1 className="matcher">Matcher</h1>
-  </div>)
-  ;
+    <div className="navbar-container">
+      <div className="malt-matcher">
+        <h1 className="malt">Malt</h1>
+        <h1 className="matcher">Matcher</h1>
+      </div>
+      <div>
+        <h4 className="about">About Us</h4>
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
