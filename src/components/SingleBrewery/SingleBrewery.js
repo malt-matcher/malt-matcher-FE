aa6cd8ad80e17bf59e321d46594853fcf5ba8096
Jordan Farelli <jordanfarelli@gmail.com>
2022-10-17 11:23:52 -0600

    Adding simple passing test for CircleCI

--- src/components/SingleBrewery/SingleBrewery.js
+++ src/components/SingleBrewery/SingleBrewery.js
@@ -6,14 +6,12 @@ const SingleBrewery = () => {
   return (
     <div className="single-brewery-container">
       <div className="brewery-image-container">
-        <img src={PBR} className='beer-image'/>
+        <img src={PBR} alt='beer' className='beer-image'/>
       </div>
       <div className="brewery-info-container">
         <p className="brewery-name">Niko's Tavern</p>
         <p className="brewery-distance">13.5 Miles</p>
-        <p className="brewery-location">
-          <a>http://mapquest.link</a>
-        </p>
+        <p className="brewery-location">http://mapquest.link</p>
       </div>
     </div>
   );
