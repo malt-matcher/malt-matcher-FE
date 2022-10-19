09f4cfcdf148007ff14a97882f7ef37092c4d31e
Andrew Miller <andrew.r.miller45@gmail.com>
2022-10-19 12:08:30 -0700

    landing page test block

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,18 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+///<reference types="Cypress" />
+
+describe('Landing Page' , () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:3001')
   })
+
+  it('takes user to location and main UI', () => {
+    cy.get('h1').contains('Welcome')
+  })
+
+  it('takes user to next page of the UI', () => {
+    cy.get('button').click()
+    cy.url().should('eq', 'http://localhost:3001/maltfinder')
+  })
+  
 })
