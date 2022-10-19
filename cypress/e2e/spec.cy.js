d9cc86f9cd658f531bf687e7c1782e4c0769f961
Andrew Miller <andrew.r.miller45@gmail.com>
2022-10-19 13:53:43 -0700

    skeleton for other user pages

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,7 @@
 describe('Landing Page' , () => {
 
   beforeEach(() => {
+    cy.intercept('POST', ' https://89c5da8f-5879-4c3a-959f-101eb08a1724.mock.pstmn.io/graphql').as('request')
     cy.visit('http://localhost:3001')
   })
 
@@ -14,5 +15,25 @@ describe('Landing Page' , () => {
     cy.get('button').click()
     cy.url().should('eq', 'http://localhost:3001/maltfinder')
   })
+
+})
+
+describe('Malt Matcher Page', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3001/maltfinder')
+  })
+
+  it('has predictable user view on page render', () => {
+    cy.get('h1[class=malt]').contains('Malt')
+    cy.get('button').contains('Show')
+
+  })
+})
+
+describe('Breweries Page', () => {
+
+})
+
+describe('Single Brewery Listing Page', () => {
   
 })
