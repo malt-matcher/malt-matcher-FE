e248fc0dcc1f251a38f5b74b45610d067d7a63cd
Andrew Miller <andrew.r.miller45@gmail.com>
2022-10-19 17:52:36 -0700

    add and comment out dropdown clicking. will enable with live api

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,8 +3,7 @@
 describe('Landing Page' , () => {
 
   beforeEach(() => {
-    cy.intercept('POST', ' https://89c5da8f-5879-4c3a-959f-101eb08a1724.mock.pstmn.io/graphql').as('request')
-    cy.visit('http://localhost:3001')
+    cy.visit('http://localhost:3000')
   })
 
   it('takes user to location and main UI', () => {
@@ -13,27 +12,42 @@ describe('Landing Page' , () => {
 
   it('takes user to next page of the UI', () => {
     cy.get('button').click()
-    cy.url().should('eq', 'http://localhost:3001/maltfinder')
+    cy.url().should('eq', 'http://localhost:3000/maltfinder')
   })
 
 })
 
 describe('Malt Matcher Page', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3001/maltfinder')
+    //intercept will go here//
+    //reference https://www.youtube.com/watch?v=sTfmURpY20I for setup
+    cy.visit('http://localhost:3000/maltfinder')
+    cy.get()
+    // cy.get('select[class=]').select('IPA') add classList to dropdowns
+    // cy.get('select[class=]').select('Littleton') add classList to dropdowns
+    // cy.get('select[class=]').select('5') add classList to dropdowns
+    // cy.get('button).click()
   })
 
   it('has predictable user view on page render', () => {
     cy.get('h1[class=malt]').contains('Malt')
-    cy.get('button').contains('Show')
-
+    cy.get('button').contains('Show')=
   })
 })
 
 describe('Breweries Page', () => {
-
+  beforeEach(() => {
+    // cy.visit(http://localhost:3000/maltfinder/search/:breweryLocations)
+  })
 })
 
 describe('Single Brewery Listing Page', () => {
-  
+  beforeEach(()=> {
+    // cy.visit(http://localhost:3000/maltfinder/search/:breweryLocations:selectedBrewery)
+  })
+})
+
+describe('Error Message Page', () => {
+  cy.visit('http://localhost:3000/maltf')
+  cy.get('h2').contains('Party')
 })
