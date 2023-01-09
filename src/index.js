d569afe957ef0362a03fce6c710ed8ff3789763e
Bryce Simonds (He/Him) <bsimonds14@gmail.com>
2023-01-09 16:22:57 -0700

    Update index.js
    Attempt #1 to change URI from vercel back to heroku

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "https://malt-matcher-be.vercel.app/",
+  uri: "https://malt-matchers-be.herokuapp.com/graphql",
   cache: new InMemoryCache(),
 });
 
